937efa11c06a5323e005e55f0b2449c0abc8f0cc
Dmytro Klimenko <Frontend@Klim-6.local>
2016-02-19 17:12:31 +0200

    fixed scroll deleted

--- js/scripts.js
+++ js/scripts.js
@@ -81,7 +81,6 @@ $(document).ready(function() {
 	});
 
 
-	// new CBPGridGallery(document.getElementById('grid-gallery-top'));
 	/*===============================
 	=            GALLERY            =
 	===============================*/
@@ -469,83 +468,7 @@ $(document).ready(function() {
 	};
 
 
-	/*====================================
-	=            FIXED SCROLL            =
-	====================================*/
-	var cbpFixedScrollLayout = (function() {
-		// cache and initialize some values
-		var config = {
-			// the cbp-fbscroller´s sections
-			$sections: $('#cbp-fbscroller > section'),
-			// the navigation links
-			$navlinks: $('#cbp-fbscroller > nav:first > a'),
-			// index of current link / section
-			currentLink: 0,
-			// the body element
-			$body: $('html, body'),
-			// the body animation speed
-			animspeed: 650,
-			// the body animation easing (jquery easing)
-			animeasing: 'easeInOutExpo'
-		};
-
-		function init() {
-
-			// click on a navigation link: the body is scrolled to the position of the respective section
-			config.$navlinks.on('click', function() {
-				scrollAnim(config.$sections.eq($(this).index()).offset().top);
-				$(this).find('.magnet-navigation__icon').addClass('magnet-navigation__icon-active');
-				$(this).siblings().find('.magnet-navigation__icon').removeClass('magnet-navigation__icon-active')
-				$(this).find('use').attr('xlink:href', '#scr-top--magnet-navigation--magnet-navigation-link--active');
-				$(this).siblings().find('use').attr('xlink:href', '#scr-top--magnet-navigation--magnet-navigation-link');
-				return false;
-			});
-
-			// 2 waypoints defined:
-			// First one when we scroll down: the current navigation link gets updated. 
-			// A `new section´ is reached when it occupies more than 70% of the viewport
-			// Second one when we scroll up: the current navigation link gets updated. 
-			// A `new section´ is reached when it occupies more than 70% of the viewport
-			config.$sections.waypoint(function(direction) {
-				if (direction === 'down') {
-					changeNav($(this));
-				}
-			}, {
-				offset: '30%'
-			}).waypoint(function(direction) {
-				if (direction === 'up') {
-					changeNav($(this));
-				}
-			}, {
-				offset: '-30%'
-			});
-
-			// on window resize: the body is scrolled to the position of the current section
-			$(window).on('debouncedresize', function() {
-				scrollAnim(config.$sections.eq(config.currentLink).offset().top);
-			});
-
-		}
-
-		// update the current navigation link
-		function changeNav($section) {
-			config.$navlinks.eq(config.currentLink).removeClass('cbp-fbcurrent');
-			config.currentLink = $section.index('section');
-			config.$navlinks.eq(config.currentLink).addClass('cbp-fbcurrent');
-		}
-
-		// function to scroll / animate the body
-		function scrollAnim(top) {
-			config.$body.stop().animate({
-				scrollTop: top
-			}, config.animspeed, config.animeasing);
-		}
-
-		return {
-			init: init
-		};
-
-	})();
+	
 
 	
 	/*=======================================
@@ -606,6 +529,14 @@ $(document).ready(function() {
 		 	}, 'down')
 	};
 	
-	  $(".content").snapscroll();
+	/*==========================================
+	=            Sections scrolling            =
+	==========================================*/
+  $(".content").snapscroll();
+
+  /*===========================================
+  =            Sections navigation            =
+  ===========================================*/
+  
 });
 
